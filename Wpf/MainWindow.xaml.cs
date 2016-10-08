925278da4250107cd2be06215856da8c5fee92d9
beto rodriguez <rodriguezo.alberto@gmail.com>
2016-10-08 18:06:04 -0500

    Add project files.

---
+++ Wpf/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private bool _noF;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            ((MainWindowVm) DataContext).Go();
+        }
+
+        private void Animated_OnChecked(object sender, RoutedEventArgs e)
+        {
+            if (!_noF)
+            {
+                _noF = !_noF;
+                return;
+            }
+            MessageBox.Show("Once you go animated, you could face a visual error with axis separators");
+        }
+    }
+}
---
+++ Wpf/MainWindowVm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using LiveCharts;
+using LiveCharts.Geared;
+using LiveCharts.Wpf;
+
+namespace Wpf
+{
+    public class MainWindowVm : INotifyPropertyChanged
+    {
+        private double _points;
+        private SeriesCollection _seriesCollection;
+
+        public MainWindowVm()
+        {
+            _points = 100000d;
+            Quality = Quality.Medium;
+        }
+
+        public double Points
+        {
+            get { return _points; }
+            set
+            {
+                _points = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SeriesCollection SeriesCollection
+        {
+            get { return _seriesCollection; }
+            set
+            {
+                _seriesCollection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Quality Quality { get; set; }
+
+        public void Go()
+        {
+            Points = Math.Truncate(Points);
+
+            var ar = new double[(int) Points+1];
+
+            var r = new Random();
+            var trend = 0d;
+
+            for (var i = 0; i < Points; i++)
+            {
+                ar[i] = trend;
+
+                if (r.NextDouble() > 0.5)
+                {
+                    trend += r.NextDouble()*10;
+                }
+                else
+                {
+                    trend -= r.NextDouble()*10;
+                }
+            }
+
+            var series = new GLineSeries
+            {
+                Values = ar.AsGearedValues().WithQuality(Quality)
+            };
+
+            SeriesCollection = new SeriesCollection
+            {
+                series
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class SeriesVm
+    {
+        public string Name { get; set; }
+        public Type Type { get; set; }
+    }
+}
