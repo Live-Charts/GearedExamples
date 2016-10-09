d28a0c20867521248076108bb8041a59bc350ad2
beto rodriguez <rodriguezo.alberto@gmail.com>
2016-10-08 20:59:17 -0500

    winforms example

---
+++ WinForms/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cartesianChart1.Location = new System.Drawing.Point(0, 0);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(788, 576);
+            this.cartesianChart1.TabIndex = 0;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(499, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Use your mouse wheel to zoom in/out click hold and drag for panning, this chart c" +
+    "ontains 100,000 points";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(788, 576);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinForms/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+using LiveCharts;
+using LiveCharts.Geared;
+using LiveCharts.Wpf;
+
+namespace WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            cartesianChart1.Zoom = ZoomingOptions.X;
+            cartesianChart1.DisableAnimations = true;
+            cartesianChart1.AxisX = new AxesCollection
+            {
+                new Axis {MinRange = 10, MaxRange = 100000}
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var ar = new double[100000];
+            var r = new Random();
+            var trend = 0d;
+
+            for (var i = 0; i < 100000; i++)
+            {
+                ar[i] = trend;
+                if (r.NextDouble() < 0.5)
+                {
+                    trend += r.NextDouble()*10;
+                }
+                else
+                {
+                    trend -= r.NextDouble()*10;
+                }
+            }
+
+            cartesianChart1.Series = new SeriesCollection
+            {
+                new GLineSeries
+                {
+                    Values = ar.AsGearedValues().WithQuality(Quality.High),
+                    StrokeThickness = 1
+                }
+            };
+        }
+    }
+}
